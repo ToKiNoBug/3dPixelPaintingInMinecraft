89aede53e178d56e59e2511d01c0ca0e2641ca9e
时之虫 <62002934+ToKiNoBug@users.noreply.github.com>
2021-02-20 17:18:35 +0800

    Add files via upload
    用于读取地图文件的c艹程序

---
+++ v1.0/ReadMap.dat/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstring>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+#include "NBT.h"
+//char FileName[32]="map_152";
+int main(int argc, char** argv) {
+	char FileName[32];
+	cout<<"请输入待分析的文件名：";
+	gets(FileName);
+	cout<<endl<<"正在分析中："<<endl;
+	ReadMap(FileName);
+	RawExport();
+	Raw2Color();
+	ColorExport();
+	Color2BaseDepth();
+	BaseExport();
+	DepthExport();
+	system("pause");
+	return 0;
+}
